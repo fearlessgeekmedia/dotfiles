8d5b5dc96a260f02d32fb1b8018a6d0544de07e5
michaelaverywilliams@gmail.com <service@fearlessgeekmedia.com>
2024-05-31 22:18:25 -0400

    Added autostart files

---
+++ .config/autostart/autostart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# exec dbus-update-activation-environment --all &&
+# exec wireplumber &
+# exec pipewire &
+exec waybar &
+exec swaybg -i /home/fearlessgeek/wallpapers/spider-punk.jpg -m fill &
+exec sleep 3
+exec /usr/bin/nm-applet &
+hyprpm enable hyprbars &
+hyprpm enable borders-plus-plus &
